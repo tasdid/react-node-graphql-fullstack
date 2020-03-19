0bd8feb034fcdcef26eaf922cff48e50eb326cf9
Salman Tasdid <salmantasdid@gmail.com>
2020-03-19 10:09:26 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use('/graphql', expressGraphQL({
 
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
-const webpackConfig = require('../webpack.config.js.js.js');
+const webpackConfig = require('../webpack.config.js');
 app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
